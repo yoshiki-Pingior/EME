f3720c8d59a5fd60511252513ef26b5fa59c0274
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-06-03 12:16:13 +0000

    [add]user_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
   end
 
   def show
+
   end
 
   def edit
   end
 
   def update
+  end
+
+   private
+
+  def user_params
+     params.require(:user).permit(:email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana)
   end
 end
