b7a40edb34b82066c9ab083b6120312c3838f6a4
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-06-14 16:09:16 +0000

    [modified]user&post_page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @posts = Post.search(params[:search]).order(created_at: :desc)
+    @posts = Post.search(params[:search]).order(created_at: :desc).page(params[:page]).per(6)
   end
 
   def new
