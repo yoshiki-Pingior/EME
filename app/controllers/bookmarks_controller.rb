f737d1dd1a467c2faac068c187987d265535cf2d
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-06-16 13:26:17 +0000

    [modified]Ajax_direct_message

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,21 +3,23 @@ class BookmarksController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     bookmark = @post.bookmarks.new(user_id: current_user.id)
-    if bookmark.save
-      redirect_to request.referer
-    else
-      redirect_to request.referer
-    end
+    # if 
+    bookmark.save
+    #   redirect_to request.referer
+    # else
+    #   redirect_to request.referer
+    # end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     bookmark = @post.bookmarks.find_by(user_id: current_user.id)
-    if bookmark.present?
-        bookmark.destroy
-        redirect_to request.referer
-    else
-        redirect_to request.referer
-    end
+    # if 
+    # bookmark.present?
+    bookmark.destroy
+    #     redirect_to request.referer
+    # else
+    #     redirect_to request.referer
+    # end
   end
 end
