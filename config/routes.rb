058df410996d93ba74e2a40cc87463b0d279c967
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-06-17 16:29:24 +0000

    [modified]user_page

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,8 @@ Rails.application.routes.draw do
   end
   
   get '/boards/bookmarks' => "boards#bookmarks"
-
-
-
+  
+  get 'finder' => "finders#finder"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
