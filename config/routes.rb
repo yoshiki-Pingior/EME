a818e786a5400603d51eb67b80e3befdf256a251
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-06-23 12:52:31 +0000

    [modified]layout_all

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: "users/registrations",
+    passwords: 'users/passwords'
   }
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
