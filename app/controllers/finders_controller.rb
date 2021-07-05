68fff340f17337d31611ba6ffc7d82f056396107
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-07-05 09:32:13 +0000

    [modified]tag_color

--- app/controllers/finders_controller.rb
+++ app/controllers/finders_controller.rb
@@ -1,10 +1,12 @@
 class FindersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def finder
     @users = User.looks(params[:word]).page(params[:page]).per(10)
-    @posts = Post.looks(params[:word]).page(params[:page]).per(6)
-    @bookmarks = Bookmark.looks(params[:word]).page(params[:page]).per(6)
+    @users = @users.order(created_at: :desc).page(params[:page]).without_count.per(10)
+    @posts = Post.looks(params[:word]).order(created_at: :desc)
+    @posts = @posts.page(params[:page]).without_count.per(6)
+    @bookmarks = Bookmark.where(user_id: current_user.id).looks(params[:word]).page(params[:page]).per(6).order(post_id: :desc)
   end
 end
 
